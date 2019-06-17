f28d1fc0694c5f5df790dda0049dc2a2193d01c9
squirrelinsouthern <squirrel_000@163.com>
2019-06-18 00:42:20 +0800

    栅格 初步完成响应式 代码有点烂

--- src/col.vue
+++ src/col.vue
@@ -7,6 +7,17 @@
 </template>
 
 <script>
+    let validator = (value) => {
+        let keys = Object.keys(value)
+        let valid = true
+        keys.forEach(key => {
+            if (!['span', 'offset'].includes(key)) {
+                valid = false
+            }
+        })
+        return valid
+    }
+
 export default {
     name:'mo-col',
     props:{
@@ -16,19 +27,11 @@ export default {
         offset:{
             type:Number
         },
-        phone:{
-            type:Object,
-            validator(obj){
-                let keys = Object.keys(obj)
-                let valid = true
-                keys.forEach((key)=>{
-                    if(!['span','offset'].includes(key)){
-                        valid = false
-                    }
-                })
-             return valid
-            }
-        }
+        phone: {type: Object, validator},
+        ipad: {type: Object, validator},
+        narrowPc: {type: Object, validator},
+        pc: {type: Object, validator},
+        widePc: {type: Object, validator}
     },
     data(){
         return{
@@ -42,15 +45,45 @@ export default {
         }
     },
     computed:{
+        colClass(){
+            let {span, offset, phone, ipad, narrowPc, pc, widePc} = this
+            let phoneClass = []
+            let ipadClass = []
+            let narrowPcClass = []
+            let pcClass = []
+            let widePcClass = []
+
+            if (phone) {
+                phoneClass = [`col-phone-${phone.span}`]
+            }
+            if (ipad){
+                ipadClass = [`col-ipad-${ipad.span}`]
+            }
+            if (narrowPc) {
+                narrowPcClass = [`col-narrowPc-${narrowPc.span}`]
+            }
+            if(pc){
+                pcClass = [`col-pc-${pc.span}`]
+            }
+            if(widePc){
+                widePcClass = [`col-widePc-${widePc.span}`]
+            }
+
+            return [
+                span &&` col-${span}`,
+                offset && `offset-${offset}`,
+                ...phoneClass,
+                ...ipadClass,
+                ...narrowPcClass,
+                ...pcClass,
+                ...widePcClass
+            ]
+        },
         colStyle(){
             return{
                 paddingLeft: this.gutter / 2 + 'px',
                 paddingRight: this.gutter / 2+ 'px',
             }
-        },
-        colClass(){
-            let {span, offset} = this
-            return [span &&`col-${span}`,offset &&`offset-${offset}`]
         }
     }
 }
@@ -58,7 +91,6 @@ export default {
 
 <style scoped lang="scss" type="text/scss">
     .col{
-        /*background-color: #d8ecff;*/
         width: 50%;
         height: 30px;
     }
@@ -73,6 +105,77 @@ export default {
         .#{$class}#{$n} {
             margin-left: ( $n / 24 ) * 100%;
 
+        }
+    }
+
+    @media (max-width:576px) {
+        $class: col-phone-;
+        @for $n from 1 through 24 {
+            .#{$class}#{$n} {
+                width: ( $n / 24 ) * 100%;
+            }
+        }
+        $class: offset-phone-;
+        @for $n from 1 through 24 {
+            .#{$class}#{$n} {
+                width: ( $n / 24 ) * 100%;
+            }
+        }
+    }
+    @media (min-width:577px) and (max-width:768px){
+        $class: col-ipad-;
+        @for $n from 1 through 24 {
+            .#{$class}#{$n} {
+                width: ( $n / 24 ) * 100%;
+            }
+        }
+        $class: offset-ipad-;
+        @for $n from 1 through 24 {
+            .#{$class}#{$n} {
+                width: ( $n / 24 ) * 100%;
+            }
+        }
+    }
+    @media (min-width:769px) and (max-width:992px){
+        $class: col-narrow-pc-;
+        @for $n from 1 through 24 {
+            .#{$class}#{$n} {
+                width: ( $n / 24 ) * 100%;
+            }
+        }
+        $class: offset-narrow-pc-;
+        @for $n from 1 through 24 {
+            .#{$class}#{$n} {
+                width: ( $n / 24 ) * 100%;
+            }
+        }
+    }
+    @media (min-width:993px) and (max-width:1200px){
+        $class: col-pc-;
+        @for $n from 1 through 24 {
+            .#{$class}#{$n} {
+                width: ( $n / 24 ) * 100%;
+            }
+        }
+        $class: offset-pc-;
+        @for $n from 1 through 24 {
+            .#{$class}#{$n} {
+                width: ( $n / 24 ) * 100%;
+            }
+        }
+    }
+    @media (min-width:1201px) {
+        $class: col-wide-pc-;
+        @for $n from 1 through 24 {
+            .#{$class}#{$n} {
+                width: ( $n / 24 ) * 100%;
+            }
+        }
+        $class: offset-wide-pc-;
+        @for $n from 1 through 24 {
+            .#{$class}#{$n} {
+                width: ( $n / 24 ) * 100%;
+            }
         }
     }
     
